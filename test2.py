cbc275d631118352844e85e7ed24251c1882e017
Thinula Jayavihan <109309014+edmthinula@users.noreply.github.com>
2023-12-09 08:39:15 +0530

    finale

--- test2.py
+++ test2.py
@@ -1,3 +1,46 @@
+def validate(value):
+    try:
+        value = int(value)
+    except ValueError:
+        print("integer need")
+        return  value,False 
+    else:
+        if value in range(0,121,20):
+            return value,True
+        else :
+            print("not in range")
+            return value,False
+while True:      
+    while True:
+        p = input("pass : ")
+        x = validate(p)
+        print(type(x[0]))
+        if x[1] :
+            print(x)
+            break
+        else :
+            continue
+    while True:
+            p = input("defer : ")
+            y = validate(p)
+            print(type(y[0]))
+            if y[1] :
+                print(y)
+                break
+            else :
+                continue
+    while True:
+            p = input("fail : ")
+            t = validate(p)
+            print(type(t[0]))
+            if t[1] :
+                print(t)
+                break
+            else :
+                continue
 
-# dic = {'thi':323,
-#        'ee'}
+    if x+y+t == 120:
+         break
+    else:
+         print("total incorrect")
+
